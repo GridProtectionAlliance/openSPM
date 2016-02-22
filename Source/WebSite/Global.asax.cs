288a05b3db9a79adcc4ddc55c88b665a9c4c86aa
Ritchie Carroll <ritchiecarroll@gmail.com>
2016-02-22 14:18:50 -0500

    Updates to global.asax to include security.

--- Source/WebSite/Global.asax.cs
+++ Source/WebSite/Global.asax.cs
@@ -23,12 +23,16 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using GSF;
 using GSF.Configuration;
+using GSF.Data;
+using GSF.Identity;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using openSPM.Models;
@@ -66,8 +70,9 @@ namespace openSPM
             systemSettings.Add("DataProviderString", "AssemblyName={System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089}; ConnectionType=System.Data.SqlClient.SqlConnection; AdapterType=System.Data.SqlClient.SqlDataAdapter", "Configuration database ADO.NET data provider assembly type creation string used");
             systemSettings.Add("CompanyName", "Grid Protection Alliance", "The name of the company who owns this instance of the openMIC.");
             systemSettings.Add("CompanyAcronym", "GPA", "The acronym representing the company who owns this instance of the openMIC.");
-            systemSettings.Add("DateFormat", "MM/dd/yyyy", "The default date format to use when rendering dates.");
-            systemSettings.Add("TimeFormat", "HH:mm.ss.fff", "The default time format to use when rendering times.");
+            systemSettings.Add("DateFormat", "MM/dd/yyyy", "The default date format to use when rendering timestamps.");
+            systemSettings.Add("TimeFormat", "HH:mm.ss.fff", "The default time format to use when rendering timestamps.");
+            systemSettings.Add("DefaultSecurityRoles", "Administrator, Editor, Viewer", "The default security roles that should exist for the application.");
 
             // Load default configuration file based model settings
             global.CompanyName = systemSettings["CompanyName"].Value;
@@ -79,6 +84,12 @@ namespace openSPM
             // Load database driven model settings
             using (DataContext dataContext = new DataContext())
             {
+                // Make sure default NodeID record exists
+                ValidateDefaultNode(dataContext.Connection, Guid.Parse(systemSettings["NodeID"].Value));
+
+                // Validate default security roles exist
+                ValidateSecurityRoles(dataContext.Connection, systemSettings["DefaultSecurityRoles"].Value);
+
                 // Load global web settings
                 Dictionary<string, string> appSetting = dataContext.LoadDatabaseSettings("app.setting");
                 global.ApplicationName = appSetting["applicationName"];
@@ -157,6 +168,72 @@ namespace openSPM
                 }
 #endif
             }, DataHub.CurrentConnectionID);
+        }
+
+        /// <summary>
+        /// Data operation to validate and ensure there is a node in the database.
+        /// </summary>
+        /// <param name="database">Data connection to use for database operations.</param>
+        /// <param name="nodeID">Node ID to validate.</param>        
+        private static void ValidateDefaultNode(AdoDataConnection database, Guid nodeID)
+        {
+            // Queries
+            const string NodeCountFormat = "SELECT COUNT(*) FROM Node";
+            const string NodeInsertFormat = "INSERT INTO Node(Name, Description, Enabled) VALUES('Default', 'Default node', 1)";
+            const string NodeUpdateFormat = "UPDATE Node SET ID = {0}";
+
+            // Determine whether the node exists in the database and create it if it doesn't.
+            int nodeCount = database.ExecuteScalar<int?>(NodeCountFormat) ?? 0;
+
+            if (nodeCount == 0)
+            {
+                database.ExecuteNonQuery(NodeInsertFormat);
+                database.ExecuteNonQuery(NodeUpdateFormat, database.Guid(nodeID));
+            }
+
+        }
+
+        /// <summary>
+        /// Validates security roles for all defined nodes.
+        /// </summary>
+        /// <param name="database">Data connection to use for database operations.</param>
+        /// <param name="defaultSecurityRoles">Default security roles that should exist.</param>        
+        private static void ValidateSecurityRoles(AdoDataConnection database, string defaultSecurityRoles)
+        {
+            // Queries
+            const string RoleCountFormat = "SELECT COUNT(*) FROM ApplicationRole WHERE NodeID = {0} AND Name = {1}";
+
+            if (string.IsNullOrEmpty(defaultSecurityRoles))
+                defaultSecurityRoles = "Administrator, Editor, Viewer";
+
+            string[] roles = defaultSecurityRoles.Split(',').Select(role => role.Trim()).Where(role => !string.IsNullOrEmpty(role)).ToArray();
+
+            // For each Node in new database make sure all roles exist
+            DataTable dataTable = database.RetrieveData("SELECT ID FROM Node");
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Guid nodeID = row.ConvertField<Guid>("ID");
+
+                foreach (string role in roles)
+                    if ((database.ExecuteScalar<int?>(RoleCountFormat, database.Guid(nodeID), role) ?? 0) == 0)
+                        AddRolesForNode(database, nodeID, role);
+            }
+        }
+
+        /// <summary>
+        /// Adds role for newly added node, e.g., Administrator, Editor, Viewer.
+        /// </summary>
+        /// <param name="database">Data connection to use for database operations.</param>
+        /// <param name="nodeID">Node ID to which roles are being assigned.</param>
+        /// <param name="roleName">Name of role to be added.</param>
+        private static void AddRolesForNode(AdoDataConnection database, Guid nodeID, string roleName)
+        {
+            // Queries
+            const string InsertRoleFormat = "INSERT INTO ApplicationRole(Name, Description, NodeID, UpdatedBy, CreatedBy) VALUES('{0}', '{0} Role', {{0}}, {{1}}, {{2}})";
+
+            string currentUserSID = UserInfo.UserNameToSID(UserInfo.CurrentUserID);
+            database.ExecuteNonQuery(string.Format(InsertRoleFormat, roleName), database.Guid(nodeID), currentUserSID, currentUserSID);
         }
     }
 }

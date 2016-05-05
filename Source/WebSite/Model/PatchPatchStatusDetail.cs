bd19e97247f9f86f6da467faa92c0b5d8c2c0be0
Billy Ernest <bernest@gridprotectionalliance.org>
2016-05-05 16:28:16 -0400

    Added a check mark glyphicon for expedited status in active patches.

--- Source/WebSite/Model/PatchPatchStatusDetail.cs
+++ Source/WebSite/Model/PatchPatchStatusDetail.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using GSF.Data.Model;
 
 namespace openSPM.Model
 {
@@ -11,11 +12,14 @@ namespace openSPM.Model
     {
         public int PatchID { get; set; }
         public int BusinessUnitID { get; set; }
+
+        [PrimaryKey]
         public int PatchStatusID { get; set; }
         public string PatchMnemonic { get; set; }
         public int ImpactKey { get; set; }
         public DateTime VendorReleaseDate { get; set; }
         public DateTime EvaluationDeadline { get; set; }
-        public int PatchStatusKey { get; set; }
+        public int PatchStatusKey { get; set; } 
+        public bool IsExpedited { get; set; }
     }
 }

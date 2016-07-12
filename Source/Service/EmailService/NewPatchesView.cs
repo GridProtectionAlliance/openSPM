9b579c2525b4ab7a11fc944c3b38e9fc282c7e0e
Billy Ernest <bernest@gridprotectionalliance.org>
2016-07-12 15:38:29 -0400

    Updated email service to send email when a new patch or plan is entered.

---
+++ Source/Service/EmailService/NewPatchesView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GSF.Data.Model;
+
+namespace EmailService
+{
+    [TableName("NewPatchesView")]
+    public class NewPatchesView: PendingAssessmentViolations
+    {
+
+    }
+}

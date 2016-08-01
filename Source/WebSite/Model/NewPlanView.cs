f7485d21e2fc98ce0d8df35906b181e43d15ca11
Billy Ernest <bernest@gridprotectionalliance.org>
2016-08-01 10:25:12 -0400

    Added views to see coming email notifications

---
+++ Source/WebSite/Model/NewPlanView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GSF.Data.Model;
+
+namespace EmailService
+{
+    [TableName("NewPlanView")]
+    public class NewPlanView
+    {
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public Guid UserAccountID { get; set; }
+        public int ID { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
